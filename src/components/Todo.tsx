daf4329aac7457a4f862682e840c45ee5d401990
Eunji Jo <eunjijo@Eunjiui-MacBookPro.local>
2022-11-15 18:19:36 +0900

    Feat : 초기 설정 Fin

---
+++ src/components/Todo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import * as S from "./Style";
+
+const Todo = () => {
+  return <S.Test />;
+};
+
+export default Todo;
---
+++ src/pages/todo/Todo.tsx
@@ -0,0 +1,9 @@
+import TodoPage from "components/TodoPage";
+/** @jsxImportSource @emotion/react */
+import React from "react";
+
+const Todo = () => {
+  return <TodoPage />;
+};
+
+export default Todo;
